23c255bd077098eb2013e3f5d93e873b23b50147
joelkoz <joel.kozikowski@gmail.com>
2020-12-05 23:08:33 -0500

    Remove Typecast type (its now part of SensESP)

--- src/transforms/typecast.cpp
+++
@@ -1,9 +0,0 @@
-#include "transforms/typecast.h"
-
-BoolToInt::BoolToInt() : Typecast<bool, int>([](bool in) -> int { return in ? 1 : 0; })
-{
-}
-
-IntToBool::IntToBool() : Typecast<int, bool>([](int in) -> int { return in != 0; })
-{
-}
